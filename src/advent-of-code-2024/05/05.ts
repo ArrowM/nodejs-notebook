c1c22f63c48d8c20060ff8abfef0402c29249580
ArrowM <arrowman.6677@gmail.com>
2024-12-26 14:22:25 -0600

    05

--- src/advent-of-code-2024/05/05.ts
+++ src/advent-of-code-2024/05/05.ts
@@ -5,15 +5,56 @@ const input = input05.split("\n");
 
 /** Part 1 **/
 
-function solve1(input: string) {
+const brIdx = input.indexOf("");
+const rules = input.slice(0, brIdx).map(rule => rule.split("|").map(n => parseInt(n)));
+const updates = input.slice(brIdx + 1).map(update => update.split(",").map(n => parseInt(n)));
 
+function isValidUpdate(update: number[]) {
+	for (const [beforeNum, afterNum] of rules) {
+		const beforeIdx = update.indexOf(beforeNum);
+		const afterIdx = update.indexOf(afterNum);
+		if ([beforeIdx, afterIdx].includes(-1)) {
+			continue;
+		}
+		if (beforeIdx > afterIdx) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function solve1() {
+	// sum middle numbers of valid updates
+	return updates
+		.filter(update => isValidUpdate(update))
+		.reduce((total, update) => total + update[Math.floor(update.length / 2)], 0);
 }
 
 
 /** Part 2 **/
 
-function solve2(input: string) {
+const numToAfterMap: { [key: number]: Set<number> } = {};
+rules.forEach(([bef, aft]) => {
+	numToAfterMap[bef] = numToAfterMap[bef] ?? new Set();
+	numToAfterMap[bef].add(aft);
+});
 
+function compareNum(a: number, b: number) {
+	if (numToAfterMap[a]?.has(b)) {
+		return -1;
+	}
+	if (numToAfterMap[b]?.has(a)) {
+		return 1;
+	}
+	return 0;
+}
+
+function solve2() {
+	// sum middle numbers of originally invalid updates after they have been corrected
+	const invalidUpdates = updates.filter(update => !isValidUpdate(update));
+	return invalidUpdates
+		.map(update => update.sort((a, b) => compareNum(a, b)))
+		.reduce((total, update) => total + update[Math.floor(update.length / 2)], 0);
 }
 
 /** Solver **/
